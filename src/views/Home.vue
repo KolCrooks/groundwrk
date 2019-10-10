b077cb0589234703c7e97410f5afc382681810b3
Sean McHale <seanryanmchale@yahoo.com>
2019-10-09 21:11:29 -0400

    init commit

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/jb.png" />
+    <HelloWorld msg="Testing editing the headers" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "home",
+  components: {
+    HelloWorld
+  }
+};
+</script>
