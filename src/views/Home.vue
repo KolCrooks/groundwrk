344f30ac2324aed790c0deb5fc3e1251717cb0c0
greenskiearth <seanryanmchale@yahoo.com>
2019-10-19 19:06:52 -0400

    deleting some stuff

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/jb.png" />
-    <HelloWorld msg="Testing editing the headers" />
+    
+    <h1>Home Page</h1>
   </div>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-  name: "home",
-  components: {
-    HelloWorld
-  }
-};
-</script>
