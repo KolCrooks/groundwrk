87046e1b643d3ddbc064ab79026dffb314fe8e26
greenskiearth <seanryanmchale@yahoo.com>
2019-10-27 22:13:02 -0400

    changing format with views

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    
+    <router-link to="grades"><img src = "./assets/redCalendar.png"></router-link>
+    <router-link to = "calendar">Calendar</router-link>
+    <router-link to = "profile">Profile</router-link>
     <router-view />
   </div>
 </template>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,9 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import calendar from "./views/calendar.vue";
+import grades from "./views/grades.vue";
+import profile from "./views/profile.vue";
 
 Vue.use(Router);
 
@@ -11,5 +14,20 @@ export default new Router({
       name: "home",
       component: Home
     },
+    {
+      path: "/calendar",
+      name: "calendar",
+      component: calendar
+    },
+    {
+      path: "/grades",
+      name: "grades",
+      component: grades
+    },
+    {
+      path: "/profile",
+      name: "profile",
+      component: profile
+    }
   ]
 });
