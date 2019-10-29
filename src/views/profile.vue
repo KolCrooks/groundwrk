ad8997b911ece9c0d012d48d69118ee1708af6e3
Kol Crooks <kolcrooks10@gmail.com>
2019-10-28 22:42:09 -0400

    profile page example

--- src/views/profile.vue
+++ src/views/profile.vue
@@ -1,7 +1,24 @@
 <template>
-  <h1>Profile</h1>
+  <div>
+    <h1>Profile</h1>
+    <q-tabs v-model="tab" class="text-teal">
+      <q-tab name="calendar" icon="calendar_today" label="calendar" />
+      <q-tab name="alarms" icon="alarm" label="Alarms" />
+      <q-tab name="movies" icon="movie" label="Movies" />
+    </q-tabs>
+    <q-card class="my-card">
+      <q-card-section>{{ lorem }}</q-card-section>
+    </q-card>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      lorem:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    };
+  }
+};
 </script>
