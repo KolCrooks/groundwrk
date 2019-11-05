d3773565d335f50865f4487dc7d0df009db19111
greenskiearth <seanryanmchale@yahoo.com>
2019-11-05 08:33:50 -0500

    taking out accountButton

--- src/views/profile.vue
+++ src/views/profile.vue
@@ -1,13 +1,13 @@
 <template>
   <div> 
     <h1>Profile</h1>
-    <accountButton></accountButton>
+    
   </div>
   
 </template>
 
 <script>
-import accountButton from './components/accountButton.vue'
+//import accountButton from './components/accountButton.vue'
 
 export default {
     components:{
