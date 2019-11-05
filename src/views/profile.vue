44defd2a08c7afaaf0ab6bb710b2dc089239dd16
greenskiearth <seanryanmchale@yahoo.com>
2019-11-04 22:03:08 -0500

    adding to do list

--- src/views/profile.vue
+++ src/views/profile.vue
@@ -1,18 +1,54 @@
 <template>
-  <div> 
-    <h1>Profile</h1>
-    <!-- <accountButton></accountButton> -->
-  </div>
-  
+
+<q-layout>
+<q-page-container>
+    
+<q-page class="q-pa-sm">
+    <q-list bordered>
+      <q-item 
+      v-for="task in tasks"
+      :key="task.id"
+      v-ripple>
+        <q-item-section side top>
+          <q-checkbox v-model="task.completed" />
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>{{task.name}}</q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-list>
+</q-page>
+</q-page-container>
+</q-layout>
 </template>
 
 <script>
-// import accountButton from './components/accountButton.vue'
-
-export default {
-    components:{
-        // accountButton
+export default{
+    data(){
+        return{
+            tasks: [
+            {
+            id: 1,
+            name: 'Study for Calc',
+            completed: false
+             },
+             {
+            id: 2,
+            name: 'Study for Chem',
+            completed: false
+             },
+            {
+            id: 3,
+            name: 'Study for Biology',
+            completed: false
+            }
+            ]
+        }
     }
-  
 }
 </script>
+
+<style>
+
+</style>
