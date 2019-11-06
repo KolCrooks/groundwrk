b55b8404b80363e7179a799a9fdd53df06f6bf70
Kol Crooks <kolcrooks10@gmail.com>
2019-11-05 20:53:42 -0500

    google login stuff

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import { LoaderPlugin } from "vue-google-login";
+import GSignInButton from 'vue-google-signin-button';
 
 import "./quasar";
 
@@ -14,6 +14,4 @@ new Vue({
   render: h => h(App)
 }).$mount("#app");
 
-Vue.use(LoaderPlugin, {
-  client_id: '356679115182-60a1e40t5i2neo5l2472l0sbtre8ju9v.apps.googleusercontent.com'
-});
+Vue.use(GSignInButton);
