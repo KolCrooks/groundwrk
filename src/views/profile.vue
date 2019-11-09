84a9e7b8a2059c260c9b3f1d1efda25d899a9f4b
Kol Crooks <kolcrooks10@gmail.com>
2019-11-09 11:07:24 -0500

    profile stuff + google api start

--- src/views/profile.vue
+++ src/views/profile.vue
@@ -1,41 +1,43 @@
 <template>
-  <q-layout>
-    <q-page-container>
-      <q-page class="q-pa-sm">
-        <q-list separator bordered>
-          <q-item
-            v-for="task in tasks"
-            :key="task.id"
-            @click="task.completed = !task.completed"
-            :class="!task.completed ? 'bg-red-1' : 'bg-green-1'"
-            clickable
-            v-ripple
-          >
-            <q-item-section side top>
-              <q-checkbox v-model="task.completed" />
-            </q-item-section>
+  <div>
+    <q-toolbar class="toolbar bg-secondary text-white">
+      <q-btn flat round dense>
+        <q-icon name="r_menu" />
+      </q-btn>
+      <q-toolbar-title>Profile</q-toolbar-title>
+    </q-toolbar>
+    <q-list separator bordered>
+      <q-item
+        v-for="task in tasks"
+        :key="task.id"
+        @click="task.completed = !task.completed"
+        :class="!task.completed ? 'bg-red-1' : 'bg-green-1'"
+        clickable
+        v-ripple
+      >
+        <q-item-section side top>
+          <q-checkbox v-model="task.completed" />
+        </q-item-section>
 
-            <q-item-section>
-              <q-item-label :class="{'text-strikethrough' : task.completed}">{{task.name}}</q-item-label>
-            </q-item-section>
-            <q-item-section side>
-              <div class="row">
-                <div class="column justify-center">
-                  <q-icon name="event" size="18px" class="q-mr-xs" />
-                </div>
-                <div class="column">
-                  <q-item-label class="row justify-end" caption>{{task.dueDate}}</q-item-label>
-                  <q-item-label class="row justify-end" caption>
-                    <small>{{task.studyTime}}</small>
-                  </q-item-label>
-                </div>
-              </div>
-            </q-item-section>
-          </q-item>
-        </q-list>
-      </q-page>
-    </q-page-container>
-  </q-layout>
+        <q-item-section>
+          <q-item-label :class="{'text-strikethrough' : task.completed}">{{task.name}}</q-item-label>
+        </q-item-section>
+        <q-item-section side>
+          <div class="row">
+            <div class="column justify-center">
+              <q-icon name="event" size="18px" class="q-mr-xs" />
+            </div>
+            <div class="column">
+              <q-item-label class="row justify-end" caption>{{task.dueDate}}</q-item-label>
+              <q-item-label class="row justify-end" caption>
+                <small>{{task.studyTime}}</small>
+              </q-item-label>
+            </div>
+          </div>
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </div>
 </template>
 
 <script>
