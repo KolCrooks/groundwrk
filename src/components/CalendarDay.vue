161c4ae34754c062d216c96b7d9860503bf29ddd
BuildTools <unconfigured@null.spigotmc.org>
2019-11-10 14:44:39 -0500

    drop down start

--- src/components/CalendarDay.vue
+++ src/components/CalendarDay.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="dayContents">
     <div class="day">{{month}} {{ day }}</div>
-    <q-item class="task" v-ripple clickable v-for="task in tasks" :key="task.name">
-      <q-item-section>{{ task.name }}</q-item-section>
-    </q-item>
+    <task class="task" v-ripple clickable v-for="task in tasks" :name="task.name"></task>
   </div>
 </template>
 
 <script>
+import task from "@/components/task.vue";
 export default {
+  components: { task },
   data() {
     return {
       tasks: [
@@ -45,20 +45,22 @@ export default {
 
 <style lang="scss" scoped>
 .dayContents {
-  background: #04365a63;
+  background: #2d2926;
   margin: 3% 2.5% 3% 2.5%;
   padding: 2%;
   min-height: 18em;
+  max-width: 95%;
   border-radius: 2px;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.46);
 }
 .day {
   width: 100%;
   text-align: right;
-  color: 'primary';
+  color: white;
   font-size: 200%;
 }
 .task {
-  color: 'secondary';
+  color: "secondary";
+  border-radius: 4px;
 }
 </style>
