0c60617d5976916b08fc8438bd0fadd5e48bbacb
Kol Crooks <kolcrooks10@gmail.com>
2019-11-10 19:18:42 -0500

    PWA now a thing

--- vue.config.js
+++ vue.config.js
@@ -6,5 +6,12 @@ module.exports = {
     disableHostCheck: true,
     port: 8080
   },
+  pwa: {
+    name: "GroundWrk",
+    themeColor: "#4DBA87",
+    msTileColor: "#000000",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black"
+  },
   transpileDependencies: [/[\\\/]node_modules[\\\/]quasar[\\\/]/]
 };
